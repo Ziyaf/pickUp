983e432d4184edebe6d215e4a9812f8025922540
Ziyaf <ziyafms808@gmail.com>
2020-01-15 23:41:29 +0530

    added entry point

--- app/src/main/java/com/example/pickup/MainActivity.java
+++ app/src/main/java/com/example/pickup/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.pickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button driver, customer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        driver = findViewById(R.id.Driver);
+        customer = findViewById(R.id.Customer);
+
+        driver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
